7ff4919353e0afd428cfdb90e6e9b16dc5848926
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-06-25 08:36:04 +0530

    Connect to the database using sqlalchemy

--- Blog/main.py
+++ Blog/main.py
@@ -10,3 +10,10 @@ app = FastAPI()
 # def create_blog(title,content): # instead of paramter we use pydantic model for req.body
 def create_blog(req:schemas.Blog): # instead of paramter we use pydantic model for req.body
     return {"title":req.title,"content":req.content}
+
+
+
+
+
+# 6. Pydantic Model (Schema) Note: FastAPI doesn't require you to use a SQL(relational) DB] but i can use any relationalDB
+# 7. Connecting to DB
