3b18028b9fc2fd63b6075ec3b2def954944ed6e6
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-06-25 20:55:28 +0530

    Delete route use here synchronize_session=false we use here for fast delete it help stop session to do anything first commit or refresh db so data delete from db

--- Blog/main.py
+++ Blog/main.py
@@ -59,7 +59,32 @@ def get_single_blog(blogId:int,db:Session = Depends(get_db)):
 # we use here "HTTPException" because we want to return 404 status code
 
 
+
+
+@app.delete('/delete-blog',status_code=status.HTTP_200_OK)
+def delete_blog(blogId:int,db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blogId).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Blog not found")
+    db.query(models.Blog).filter(models.Blog.id == blogId).delete(synchronize_session=False)
+    db.commit()
+    return {"message":"Blog deleted successfully"} 
+# we use here "HTTPException" because we want to return 404 status code
+# we use here "synchronize_session=False" because we want to delete the blog from database
+# and we don't want to update the session
+# 1️⃣  Session खोलो
+# 2️⃣  मान लो पहले कुछ users बनाकर commit कर चुके हो…
+# 3️⃣  अब user id == 5 डिलीट करना है
+# 4️⃣  user id == 5 को delete करो
+# 5️⃣  जहाँ id 5 है
+# 6️⃣  DELETE statement रन करो
+# 7️⃣  session को अभी न छेड़ो
+# 8️⃣  matlab ye hai ki session ko abhi kuch mat kar phle jo dlete kra usko commit kro aur refresh kar do takki session up to date ho jaye
+
+
+
 # 6. Pydantic Model (Schema) Note: FastAPI doesn't require you to use a SQL(relational) DB] but i can use any relationalDB
 # 7. Connecting to DB
 # 8. Models & Tables
 # 9. Exception Code & Status Code
+# 10. Delete a Blog
